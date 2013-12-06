302ce0718ef80583a030464db3eed76c3ada4371
Etienne Lamy <etienne.lamydlc@gmail.com>
2013-12-06 09:07:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikestand::Application.config.secret_token = '12e2b4a758432ca6a2d9065ef55ea8e6fe9f7ac69171f7bf763b280437add86e46b13bac171f4718bb969523177dfb326e0a8b7b50e36394ac89547d9338ce77'
