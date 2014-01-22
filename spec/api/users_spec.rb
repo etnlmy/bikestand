f4db1c84f87027bd0d4a5c6fe60c2a5eef3354ab
Etienne Lamy <etienne.lamydlc@gmail.com>
2014-01-22 08:56:50 +0100

    add specs for users api

---
+++ spec/api/users_spec.rb
@@ -0,0 +1,27 @@
+describe "Contracts API" do
+
+  before(:all) do
+    @contract = FactoryGirl.create(:contract)
+    9.times { FactoryGirl.create(:contract) }
+  end 
+  after(:all) { Contract.delete_all }
+  
+  describe "index" do   
+    it "should return all the contracts as JSON response" do
+      get api_contracts_path
+      expect(response).to be_success
+      json = JSON.parse(response.body)
+      expect(json.length).to eq(10)     
+    end    
+  end
+  
+  describe "show" do
+    it "should return the right contract as JSON response" do
+      get api_contract_path(@contract)
+      expect(response).to be_success
+      json = JSON.parse(response.body)
+      expect(json["cities"]).to eq(@contract.cities) 
+    end
+  end
+  
+end
