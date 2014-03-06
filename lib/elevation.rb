7cad8162dd792d7d95dbcfcc00c3f07062cea7e7
Etienne Lamy <etienne.lamydlc@gmail.com>
2014-03-06 23:35:24 +0100

    elevation_for is now an instance method

--- lib/elevation.rb
+++ lib/elevation.rb
@@ -4,24 +4,21 @@ require 'uri'
 # Provide a method to query the Google Elevation API
 module Elevation
   extend ActiveSupport::Concern
-  
-  module ClassMethods
-  
-    # Get the elevation in meters from a location's latitude and longitude
-    #
-    # Returns a Float
-    def elevation_for(latitude, longitude)
-      base_url = 'http://maps.googleapis.com/maps/api/elevation/json'
-      elevation_uri = URI.parse(
-        "#{base_url}?locations=#{latitude},#{longitude}&sensor=false"
-        )
-      http = Net::HTTP.new(elevation_uri.host, elevation_uri.port)
-      request = Net::HTTP::Get.new(elevation_uri.request_uri)
-      response = http.request(request)
-      if response.code == '200'
-        response = JSON.parse(response.body)
-        response['results'][0]['elevation'] if response['status'] == 'OK'
-      end
+    
+  # Get the elevation in meters from a location's latitude and longitude
+  #
+  # Returns a Float
+  def elevation_for(latitude, longitude)
+    base_url = 'http://maps.googleapis.com/maps/api/elevation/json'
+    elevation_uri = URI.parse(
+      "#{base_url}?locations=#{latitude},#{longitude}&sensor=false"
+      )
+    http = Net::HTTP.new(elevation_uri.host, elevation_uri.port)
+    request = Net::HTTP::Get.new(elevation_uri.request_uri)
+    response = http.request(request)
+    if response.code == '200'
+      response = JSON.parse(response.body)
+      response['results'][0]['elevation'] if response['status'] == 'OK'
     end
   end
 end
