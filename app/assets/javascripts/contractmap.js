e547806ae3dc42a6d63ecc2cce4ba65405924b64
Etienne Lamy <etienne.lamydlc@gmail.com>
2014-07-19 00:07:02 +0200

    coding style

--- app/assets/javascripts/contractmap.js
+++ app/assets/javascripts/contractmap.js
@@ -1,33 +1,16 @@
-var contractMap = function () {
-
-  var contract, div, layer, map;
+var contractMap = (function () {
+  var stations, div, map;
 
   function stationsBounds(stations) {
     var southWest = L.latLng(
-      d3.min(stations.map(function(station) { return station.latitude; })),
-      d3.min(stations.map(function(station) { return station.longitude; }))),
-        northEast = L.latLng(
-      d3.max(stations.map(function(station) { return station.latitude; })),
-      d3.max(stations.map(function(station) { return station.longitude; })));
+        d3.min(stations.map(function (station) { return station.latitude; })),
+        d3.min(stations.map(function (station) { return station.longitude; }))
+      ),
+      northEast = L.latLng(
+        d3.max(stations.map(function (station) { return station.latitude; })),
+        d3.max(stations.map(function (station) { return station.longitude; }))
+      );
     return L.latLngBounds(southWest, northEast);
-  }
-
-  function markerForStation(station) {
-    var position = L.latLng(station.latitude, station.longitude);
-    var record = station.last_record;
-    if (!record) { throw "No record found for this station"; }
-
-    var color = ocupancyRateColorScale(
-      record.available_bike_stands / record.bike_stands, 
-      record.status);
-
-    return L.circleMarker(
-      position, 
-      {
-        radius: 4,
-        color: color,
-        fillOpacity: 0.6
-      });
   }
 
   function ocupancyRateColorScale(ocupancyRate, status) {
@@ -37,12 +20,30 @@ var contractMap = function () {
     if (ocupancyRate < 0.3) {
       return "#ca0020";
     }
-    else if (ocupancyRate >= 0.3 && ocupancyRate <= 0.7) {
+    if (ocupancyRate >= 0.3 && ocupancyRate <= 0.7) {
       return "#1a9641";
     }
-    else {
-      return "#404040";
-    }
+    return "#404040";
+  }
+
+  function markerForStation(station) {
+    var position = L.latLng(station.latitude, station.longitude),
+      record = station.last_record;
+    if (!record) { throw "No record found for this station"; }
+
+    var color = ocupancyRateColorScale(
+        record.available_bike_stands / record.bike_stands,
+        record.status
+      );
+
+    return L.circleMarker(
+      position,
+      {
+        radius: 4,
+        color: color,
+        fillOpacity: 0.6
+      }
+    );
   }
 
   var displayMap = function (s, d) {
@@ -51,15 +52,15 @@ var contractMap = function () {
 
     map = L.mapbox.map(div, 'examples.map-20v6611k');
     map.fitBounds(stationsBounds(stations));
-    var markers = 
 
-    stations.forEach(function(station) {
+    stations.forEach(function (station) {
       try {
-        markerForStation(station).addTo(map); 
+        markerForStation(station).addTo(map);
+      } catch (error) {
+        console.log(error);
       }
-      catch (error) {console.log(error)}
-    });  
+    });
 
   };
-  return {displayMap: displayMap}
-}();
+  return {displayMap: displayMap};
+}());
--- app/assets/javascripts/show_contract.js
+++ app/assets/javascripts/show_contract.js
@@ -1,23 +1,23 @@
-$(".container.contracts.show").ready(function() {
-  
+$(".container.contracts.show").ready(function () {
+
   var contract = $('.contract').data('contract');
-  
-  $.getJSON("/api/contracts/" + contract.id + "/stations", function(stations) {
+
+  function displayContractInfo(stations) {
+    var nbOpenStations = 0, message;
+    stations.forEach(function (station) {
+      var record = station.last_record;
+      if (!record) { return; }
+      if (record.status === "OPEN") { nbOpenStations++; }
+    });
+    message = nbOpenStations === stations.length ?
+        "<p> All the " + nbOpenStations + " stations are currently opened." :
+        "<p>There are " + nbOpenStations + " open stations out of " + stations.length + "</p>";
+    $("#contract-info").append(message);
+  }
+
+  $.getJSON("/api/contracts/" + contract.id + "/stations", function (stations) {
     contractMap.displayMap(stations, 'map-canvas');
     displayContractInfo(stations);
   });
 
-  function displayContractInfo(stations) {    
-    var nbOpenStations = 0;
-    stations.forEach(function(station) {
-      var record = station.last_record;
-      if (!record) { return; }
-      if (record.status == "OPEN") { nbOpenStations++; }
-    });
-    var message = nbOpenStations === stations.length ?
-      "<p> All the " + nbOpenStations + " stations are currently opened." :
-      "<p>There are " + nbOpenStations + " open stations out of " + stations.length + "</p>"
-    $("#contract-info").append(message);
-  }
 });
-
